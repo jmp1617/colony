9c2f41eba77bb7bc5013db3692faf47271ef2e8a
Jacob Potter <jmp1617@g.rit.edu>
2018-04-12 16:15:23 -0400

    added matrix copy fucntion

--- colony.asm
+++ colony.asm
@@ -631,6 +631,54 @@ done_write:
         jr      $ra
 #--------------------------------
 
+
+# Name:         copy_to_main
+# 
+# copies the temp array to the main array
+#
+# Arguments:    a0: size
+# Returns:      none
+#
+copy_to_main:
+#-------------------------------
+        addi    $sp, $sp, -36
+        sw      $ra, 0($sp)
+        sw      $s0, 4($sp)
+        sw      $s1, 8($sp)
+        sw      $s2, 12($sp)
+        sw      $s3, 16($sp)
+        sw      $s4, 20($sp)
+        sw      $s5, 24($sp)
+        sw      $s6, 28($sp)
+        sw      $s7, 32($sp)
+#-------------------------------
+        la      $s1, grid_main
+        la      $s2, grid_temp
+        la      $t0, input_data
+        lw      $s3, 0($t0)
+        mul     $s3, $s3, $s3
+        addi    $s3, $s3, -1
+copy_loop:
+        lb      $t0, 0($s2) #load
+        sb      $t0, 0($s1) #store
+        addi    $s1, $s1, 1
+        addi    $s2, $s2, 1
+        addi    $s3, $s3, -1
+        bgez    $s3, copy_loop
+#-------------------------------
+        lw      $ra, 0($sp)
+        lw      $s0, 4($sp)
+        lw      $s1, 8($sp)
+        lw      $s2, 12($sp)
+        lw      $s3, 16($sp)
+        lw      $s4, 20($sp)
+        lw      $s5, 24($sp)
+        lw      $s6, 28($sp)
+        lw      $s7, 32($sp)
+        addi    $sp, $sp, 36
+        jr      $ra
+#--------------------------------
+
  
 # Name:         init_board
 # 
